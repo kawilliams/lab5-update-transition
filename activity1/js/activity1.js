b02d4263714a53632d555169fd755af3ebd3aa9d
kawilliams <kawilliams@davidson.edu>
2025-03-03 22:43:04 -0500

    Add activity 1 and scratchwork

---
+++ activity1/js/activity1.js
@@ -0,0 +1,33 @@
+// 3. Append an SVG drawing area to our chart-area div for the visualization
+const height = 300;
+const width = 700;
+const margin = {left: 10, top: 10, right: 10, bottom: 10};
+
+// Use our height and margins
+// var svg = d3.select('#chart-area')
+    
+
+// Append a text element to the svg with the text "Orders: 0"
+// var text = d3.select('svg')
+
+
+// The function is called every time when an order comes in or an order gets processed
+// The current order queue is stored in the variable 'orders'
+
+function updateVisualization(orders) {
+    // 2. Analyze the data stream
+    console.log(orders);
+
+    
+    // 4. Create the dymamic visualization.
+    // You should be able to reuse the code from the pre-activity
+    // var circle = svg.selectAll('circle')
+
+    // Add a single line to edit the text to the number of orders
+    // svg.select...
+    
+    //circle.exit().remove();
+   
+    
+
+}
